08ae26e6c77b75af6170dfb73af8dc647dbacc0b
yashladd <yashlad.08@gmail.com>
2020-07-27 10:39:05 +0530

    Simple flask api

--- main.py
+++ main.py
@@ -27,6 +27,15 @@ video_put_args.add_argument(
     "views", type=int, help='Views of the video is required', required=True)
 
 
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument(
+    "name", type=str, help='Name of the video  is required')
+video_update_args.add_argument(
+    "likes", type=int, help='Likes of the video is required')
+video_update_args.add_argument(
+    "views", type=int, help='Views of the video is required')
+
+
 resource_fields = {
     'id': fields.Integer,
     'name': fields.String,
@@ -39,16 +48,41 @@ class Video(Resource):
     @marshal_with(resource_fields)
     def get(self, video_id):
         result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message='Could not find video with that id')
         return result
 
     @marshal_with(resource_fields)
-    def put(self, video_id):
+    def post(self, video_id):
         args = video_put_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message='Video id already exists...')
+
         video = VideoModel(
             id=video_id, name=args['name'], likes=args['likes'], views=args['views'])
         db.session.add(video)
         db.session.commit()
         return video, 201
+
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message='Videi doesnt exist...')
+
+        if args['name']:
+            result.name = args['name']
+
+        if args['views']:
+            result.views = args['views']
+
+        if args['likes']:
+            result.likes = args['likes']
+
+        db.session.commit()
+        return result
 
     def delete(self, video_id):
         video_id_dose_not_exist(video_id)
--- test.py
+++ test.py
@@ -2,19 +2,7 @@ import requests
 
 BASE = 'http://127.0.0.1:5000/'
 
-data = [
-    {'likes': 112, 'name': 'Daily dose of standup', 'views': 10000},
-    {'likes': 1243, 'name': 'Day in the life of', 'views': 10000123},
-    {'likes': 15512, 'name': 'Travel Guide', 'views': 1123124},
-    {'likes': 657324124, 'name': 'Are we living in a simulation', 'views': 529352324},
-]
 
-for i in range(len(data)):
-    response = requests.put(
-        BASE + 'video/' + str(i), data[i])
-    print(response.json())
-
-input()
-response = requests.get(BASE + 'video/0')
+response = requests.patch(BASE + 'video/0', {})
 
 print(response.json())
